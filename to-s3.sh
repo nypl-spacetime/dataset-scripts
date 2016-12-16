b54dd3819931497fd4ecc81cceb607e1ddd52208
Bert Spaan <bert.spaan@gmail.com>
2016-12-16 18:22:34 -0500

    cd to dir before zipping, and create sample geojson

--- to-s3.sh
+++ to-s3.sh
@@ -5,8 +5,18 @@ DATA_DIR=/Users/bertspaan/data/spacetime/etl/transform
 for dir in $DATA_DIR/*; do
   if [[ -d $dir ]]; then
     dataset="$(basename $dir)"
+
+    # Convert NDJSON to GeoJSON using spacetime-cli
     spacetime-to-geojson $dir/$dataset.objects.ndjson > $dir/$dataset.geojson
-    zip -r $dir/$dataset.zip $dir/
+
+    # Create sample GeoJSON with at most 100 objects
+    gshuf -n 100 $dir/$dataset.objects.ndjson | spacetime-to-geojson > $dir/$dataset.sample.geojson
+
+    # Create ZIP file with all dataset files, remove existing ZIP file first
+    cd $dir
+    rm -f .DS_Store
+    rm -f $dataset.zip
+    zip -r $dataset.zip ./
   fi
 done
 
