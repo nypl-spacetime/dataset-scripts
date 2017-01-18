a9293501a3546b669d1dd25133503b0161007fcd
Bert Spaan <bert.spaan@gmail.com>
2017-01-18 17:37:53 -0500

    prepare files in new dir

--- to-s3.sh
+++ to-s3.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-DATA_DIR=/Users/bertspaan/data/spacetime/etl/transform
+SOURCE_DIR=/Users/bertspaan/data/spacetime/etl/transform
 
-for dir in $DATA_DIR/*; do
+TARGET_BASE=/Users/bertspaan/data/spacetime
+TARGET_DIR=$TARGET_BASE/to-s3
+
+# Empty TARGET_DIR
+rm -rf $TARGET_DIR
+mkdir $TARGET_DIR
+
+# Copy all dataset subdirectories from SOURCE_DIR to TARGET_DIR
+# TODO: only *.ndjson and *.json
+cp -r $SOURCE_DIR/* $TARGET_DIR
+
+for dir in $TARGET_DIR/*; do
   if [[ -d $dir ]]; then
     dataset="$(basename $dir)"
 
@@ -14,12 +25,10 @@ for dir in $DATA_DIR/*; do
 
     # Create ZIP file with all dataset files, remove existing ZIP file first
     cd $dir
-    rm -f .DS_Store
-    rm -f $dataset.zip
     zip -r $dataset.zip ./
   fi
 done
 
-aws s3 sync $DATA_DIR s3://spacetime-nypl-org/datasets/ --exclude "*" \
---include "*.json" --include "*.ndjson" --include "*.zip" --include "*.geojson" \
---delete --profile spacetime
+aws s3 sync $TARGET_DIR s3://spacetime-nypl-org/datasets/ --exclude "*" \
+  --include "*.json" --include "*.ndjson" --include "*.zip" --include "*.geojson" \
+  --delete --profile spacetime
