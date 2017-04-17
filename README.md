030e5bfcbe5fe3455e01e20f322cdd2648f75684
Bert Spaan <bert.spaan@gmail.com>
2017-04-17 15:30:13 -0400

    updated README - and to-s3.sh now only publishes single dataset

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Scripts for NYC Space/Time Directory
 
-Prerequisite: install [spacetime-config](https://github.com/nypl-spacetime/spacetime-config) and set the `etl.outputDir` configuration option. See [spacetime-etl](https://github.com/nypl-spacetime/spacetime-etl) for more information.
+## Publish dataset to S3
 
-- [`to-s3.sh`](to-s3.sh): upload ETL data to S3 (and create ZIP and GeoJSON files)
+[`to-s3.sh`](to-s3.sh) uploads a single NYC Space/Time Directory datasetto S3. The script also creates a GeoJSON file from the NDJSON objects file, and zips the dataset.
+
+## Prerequisites
+
+1. First, install [spacetime-config](https://github.com/nypl-spacetime/spacetime-config) and set the `etl.outputDir` configuration option. See [spacetime-etl](https://github.com/nypl-spacetime/spacetime-etl) for more information.
+2. Install [spacetime-cli](https://github.com/nypl-spacetime/spacetime-cli)
+3. Install [aws-cli](https://github.com/aws/aws-cli)
+4. Add AWS credentials to [`~/.aws/credentials`](http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html#cli-config-files), using the `spacetime` profile:
+
+```
+[spacetime]
+echo aws_access_key_id = AWS_ACCESS_KEY_ID
+echo aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+```
+
+### Usage
+
+To publish a single dataset to S3, run:
+
+    ./to-s3 DATASET.STEP
+
+For example:
+
+    ./to-s3 mapwarper.transfor
--- to-s3.sh
+++ to-s3.sh
@@ -1,52 +1,55 @@
 #!/bin/bash
 
-SPACETIME_ETL_DIR=`spacetime-config etl.outputDir`
+ETL_OUTPUT_DIR=`spacetime-config etl.outputDir`
 
-: "${SPACETIME_ETL_DIR:?Please install spacetime-config and set the etl.outputDir configuration option}"
+: "${ETL_OUTPUT_DIR:?Please install spacetime-config and set the etl.outputDir configuration option}"
 
-STEPS=( transform aggregate )
-TARGET_DIR=/tmp/spacetime-to-s3
+re="(.*)\.(.*)"
+if [[ $1 =~ $re ]]; then
+  DATASET=${BASH_REMATCH[1]}
+  STEP=${BASH_REMATCH[2]}
+else
+  echo "Please supply one command-line argument of the following form: DATASET.STEP" >&2
+  exit
+fi
+
+SOURCE_DIR=$ETL_OUTPUT_DIR/$STEP/$DATASET
+
+if ! [[ -d "$SOURCE_DIR" && -x "$SOURCE_DIR" ]]; then
+  echo "Directory does not exist: $SOURCE_DIR" >&2
+  exit
+fi
+
+TARGET_DIR=/tmp/spacetime-to-s3/$DATASET
 
 # Empty TARGET_DIR
 rm -rf $TARGET_DIR
-mkdir $TARGET_DIR
+mkdir -p $TARGET_DIR
 
-for step in "${STEPS[@]}"
-do
-  SOURCE_DIR=$SPACETIME_ETL_DIR/$step
+# Copy all dataset subdirectories from SOURCE_DIR to TARGET_DIR
+cp -p $SOURCE_DIR/* $TARGET_DIR
 
-  # Copy all dataset subdirectories from SOURCE_DIR to TARGET_DIR
-  # TODO: only *.ndjson and *.json
-  cp -p -r $SOURCE_DIR/* $TARGET_DIR
-done
+if [ -f $TARGET_DIR/$DATASET.objects.ndjson ]
+then
+  # Convert NDJSON to GeoJSON using spacetime-cli
+  spacetime-to-geojson $TARGET_DIR/$DATASET.objects.ndjson > $TARGET_DIR/$DATASET.geojson
 
-for dir in $TARGET_DIR/*; do
-  if [[ -d $dir ]]; then
-    dataset="$(basename $dir)"
+  # Create sample GeoJSON with at most 100 objects
+  exists() { type -t "$1" > /dev/null 2>&1; }
 
-    if [ -f $dir/$dataset.objects.ndjson ]
-    then
-      # Convert NDJSON to GeoJSON using spacetime-cli
-      spacetime-to-geojson $dir/$dataset.objects.ndjson > $dir/$dataset.geojson
-
-      # Create sample GeoJSON with at most 100 objects
-      exists() { type -t "$1" > /dev/null 2>&1; }
-
-      if exists gshuf; then
-        SHUF=gshuf
-      else
-        SHUF=shuf
-      fi
-
-      $SHUF -n 100 $dir/$dataset.objects.ndjson | spacetime-to-geojson > $dir/$dataset.sample.geojson
-    fi
-
-    # Create ZIP file with all dataset files
-    cd $dir
-    zip -r $dataset.zip ./
+  if exists gshuf; then
+    SHUF=gshuf
+  else
+    SHUF=shuf
   fi
-done
 
-aws s3 sync $TARGET_DIR s3://spacetime-nypl-org/datasets/ --exclude "*" \
+  $SHUF -n 100 $TARGET_DIR/$DATASET.objects.ndjson | spacetime-to-geojson > $TARGET_DIR/$DATASET.sample.geojson
+fi
+
+# Create ZIP file with all dataset files
+cd $TARGET_DIR
+zip -r $DATASET.zip ./
+
+aws s3 sync $TARGET_DIR s3://spacetime-nypl-org/datasets/$DATASET --exclude "*" \
   --include "*.json" --include "*.ndjson" --include "*.zip" --include "*.geojson" \
   --delete --profile spacetime
